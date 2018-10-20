39a5eb3d52556e678e40a9fd4c1036e01779d76c
PatrykW <wargckipat@gmail.com>
2018-10-21 00:34:09 +0200

    Broken ;__;
    I/zygote64:     at void ar.wargus.cv.core.FullscreenActivity.onCreate(android.os.Bundle) (FullscreenActivity.java:50)
    Caused by: java.lang.ClassNotFoundException: Didn't find class "android.view.View$OnUnhandledKeyEventListener" on path: ...

---
+++ core/src/main/java/ar/wargus/cv/core/FixedSurfaceView.java
@@ -0,0 +1,34 @@
+package ar.wargus.cv.core;
+
+public class FixedSurfaceView{
+	private int desiredWidth;
+	private int desiredHeight;
+	private boolean useDesiredSizes = false;
+	
+//	public FixedSurfaceView(Context context) { super(context); }
+//	public FixedSurfaceView(Context context, AttributeSet attrs) { super(context, attrs); }
+//	public FixedSurfaceView(Context context, AttributeSet attrs, int defStyleAttr) { super(context, attrs, defStyleAttr); }
+//	public FixedSurfaceView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) { super(context, attrs, defStyleAttr, defStyleRes); }
+	
+//	@Override
+//	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//		if(useDesiredSizes) {
+//			ViewGroup.LayoutParams params = this.getLayoutParams();
+//			params.height = this.desiredHeight;
+//			params.width = this.desiredWidth;
+//			this.setLayoutParams(params);
+//		}
+//	}
+//
+//	public int getDesiredWidth  () { return desiredWidth;   }
+//	public int getDesiredHeight () { return desiredHeight;  }
+//	public void setDesiredWidth (int desiredWidth)  {
+//		this.desiredWidth = desiredWidth;
+//		useDesiredSizes = true;
+//	}
+//	public void setDesiredHeight(int desiredHeight) {
+//		this.desiredHeight = desiredHeight;
+//		useDesiredSizes = true;
+//	}
+}
--- core/src/main/java/ar/wargus/cv/core/Start.java
+++ core/src/main/java/ar/wargus/cv/core/Start.java
@@ -3,93 +3,217 @@ package ar.wargus.cv.core;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Matrix;
+import android.graphics.RectF;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.params.OutputConfiguration;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.Size;
 import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.view.TextureView;
+import android.view.WindowManager;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Start {
 
 //	public static Start getInstance(CameraManager cameraManager, SurfaceView mContentView){return new Start(cameraManager, mContentView);}
 
 	private final String LOG_TAG = this.getClass().getSimpleName();
-
+	
+	private FullscreenActivity fullscreenActivity;
+	
+	private Surface surfaceTextureView;
 	private CameraManager cameraManager;
-	private Surface surface;
+	private TextureView textureView;
+	private Size prefPreviewSize;
+	
+	private TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener(){
+		@Override
+		public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+			try {
+				setupCamera(width, height);
+				openCamera();
+			} catch (CameraAccessException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		@Override
+		public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {}
+		@Override
+		public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) { return false; }
+		@Override
+		public void onSurfaceTextureUpdated(SurfaceTexture surface) {}
+	};
+	private CameraDevice.StateCallback cameraStateCallback = new CameraDevice.StateCallback() {
+		@Override
+		public void onOpened(@NonNull CameraDevice camera) {
+			try {
+				createPreviewSession(camera);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		@Override
+		public void onDisconnected(@NonNull CameraDevice camera) { camera.close(); }
+		@Override
+		public void onError(@NonNull CameraDevice camera, int error) { camera.close(); }
+	};
+	private CameraCaptureSession.StateCallback sessionStateCallback = new CameraCaptureSession.StateCallback() {
+		@Override
+		public void onConfigured(@NonNull CameraCaptureSession session) {
+			try {
+				startPreview(session);
+			} catch (Exception e) {
+			}
+		}
+		
+		@Override
+		public void onConfigureFailed(@NonNull CameraCaptureSession session) {}
+	};
+	
+	public Start() {}
 
-	public Start(CameraManager cameraManager, Surface surface) {
-		this.cameraManager = cameraManager;
-		this.surface = surface;
+	public void onResume(){
+		if(textureView.isAvailable()){
+		
+		}else{
+			textureView.setSurfaceTextureListener(surfaceTextureListener);
+		}
 	}
-
-	private CameraDevice.StateCallback cameraStateCallback;
-
-	private CameraCaptureSession.StateCallback sessionStateCallback;
-
-	private void init() {
-		cameraStateCallback = new CameraDevice.StateCallback() {
-			@Override
-			public void onOpened(@NonNull CameraDevice camera) {
-				try {
-					camera.createCaptureSession(Arrays.asList(surface), sessionStateCallback, null);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-
-			@Override
-			public void onDisconnected(@NonNull CameraDevice camera) {
-			}
-
-			@Override
-			public void onError(@NonNull CameraDevice camera, int error) {
-			}
-		};
-		sessionStateCallback = new CameraCaptureSession.StateCallback() {
-			@Override
-			public void onConfigured(@NonNull CameraCaptureSession session) {
-				try {
-					CaptureRequest.Builder builder = session.getDevice().createCaptureRequest(CameraDevice.TEMPLATE_ZERO_SHUTTER_LAG);
-					builder.addTarget(surface);
-//					builder.set(CaptureRequest.COLOR_CORRECTION_ABERRATION_MODE,    CaptureRequest.COLOR_CORRECTION_ABERRATION_MODE_FAST);
-//					builder.set(CaptureRequest.COLOR_CORRECTION_MODE,               CaptureRequest.COLOR_CORRECTION_MODE_FAST);
-//					builder.set(CaptureRequest.NOISE_REDUCTION_MODE,                CaptureRequest.NOISE_REDUCTION_MODE_FAST);
-//					builder.set(CaptureRequest.CONTROL_AF_MODE,                     CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
-//					builder.set(CaptureRequest.HOT_PIXEL_MODE,                      CaptureRequest.HOT_PIXEL_MODE_FAST);
-//					builder.set(CaptureRequest.SHADING_MODE,                        CaptureRequest.SHADING_MODE_FAST);
-//					builder.set(CaptureRequest.TONEMAP_MODE,                        CaptureRequest.TONEMAP_MODE_FAST);
-//					builder.set(CaptureRequest.EDGE_MODE,                           CaptureRequest.EDGE_MODE_ZERO_SHUTTER_LAG);
-					session.setRepeatingRequest(builder.build(), null, null);
-				} catch (Exception e) {
-				}
-			}
-
-			@Override
-			public void onConfigureFailed(@NonNull CameraCaptureSession session) {
-			}
-		};
-	}
-
 	/**
 	 *  does stuff
-	 * @param context requires application context to check camera permissions
-	 * @throws Exception
+	 * @param fullscreenActivity requires application context to check camera permissions
 	 */
-	public void doStuff(Context context) throws Exception {
-		init();
-		if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+	public void doStuff(FullscreenActivity fullscreenActivity) {
+		this.fullscreenActivity = fullscreenActivity;
+		cameraManager = (CameraManager) fullscreenActivity.getSystemService(Context.CAMERA_SERVICE);
+		textureView = fullscreenActivity.findViewById(R.id.fullscreen_content);
+		
+		if(textureView != null
+		   && textureView.getSurfaceTexture() != null)
+			surfaceTextureView = new Surface(textureView.getSurfaceTexture());
+	}
+	
+	private void setupCamera(int width, int height) throws CameraAccessException {
+		final CameraCharacteristics camChar = cameraManager.getCameraCharacteristics("0");
+		prefPreviewSize = getPreviewOutputSize(camChar);
+		transform(prefPreviewSize.getWidth(), prefPreviewSize.getHeight());
+	}
+	
+	private void openCamera() throws CameraAccessException {
+		if (ActivityCompat.checkSelfPermission(fullscreenActivity, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
 			Log.e(LOG_TAG, "Illegal state, camera access was invoked without permissions");
 			return;
 		}
 		cameraManager.openCamera("0", cameraStateCallback, null);
+	}
+	
+	private void createPreviewSession(CameraDevice camera) throws CameraAccessException {
+		camera.createCaptureSession(Arrays.asList(surfaceTextureView),
+		                            sessionStateCallback,
+		                            null);
+	}
+	
+	private void startPreview(CameraCaptureSession session) throws CameraAccessException {
+		CaptureRequest.Builder builder = session.getDevice()
+		                                        .createCaptureRequest(CameraDevice.TEMPLATE_ZERO_SHUTTER_LAG);
+		builder.addTarget(surfaceTextureView);
+		session.setRepeatingRequest(builder.build(), null, null);
+		
+	}
+	
+	private class SmartSize{
+		Size size;
+		int longer;
+		int shorter;
+		SmartSize(int width, int height){
+			this.size = new Size(width, height);
+			this.longer = Math.max(size.getWidth(),
+			                       size.getHeight());
+			this.shorter = Math.min(size.getWidth(),
+			                        size.getHeight());
+		}
+	}
+	
+	private SmartSize getDisplaySmartSize(Context context){
+		DisplayMetrics displayMetrics = new DisplayMetrics();
+		((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getMetrics(displayMetrics);
+		
+		return new SmartSize(displayMetrics.widthPixels, displayMetrics.heightPixels);
+	}
+	
+	Size getPreviewOutputSize(CameraCharacteristics camChar){
+		final double ASPECT_TOLERANCE = 0.5;
+		
+		SmartSize hdSize = new SmartSize(1080, 720);
+		SmartSize displayScreenSize = getDisplaySmartSize(fullscreenActivity);
+		boolean hdScreen = displayScreenSize.longer >= hdSize.longer
+				           || displayScreenSize.shorter >= hdSize.shorter;
+		SmartSize maxSize;
+		if(hdScreen) maxSize = displayScreenSize; else maxSize = hdSize;
+		
+		StreamConfigurationMap config = camChar.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+		
+		List<Size> allSizes = Arrays.asList(config.getOutputSizes(SurfaceTexture.class));
+		
+		Size size3 = allSizes.stream()
+		               .sorted((size1, size2)
+                               -> (size2.getHeight()
+                                   * size2.getWidth())
+		                          - (size1.getHeight()
+				                      * size1.getWidth()))
+		               .map(size
+                            -> new SmartSize(size.getWidth(),
+                                             size.getHeight()))
+//		               .filter(size
+//                               -> size.longer <= maxSize.longer
+//		                          && size.shorter <= maxSize.shorter)
+		               .filter(size
+				               -> {double d = Math.abs(((double) size.longer
+						                             /size.shorter)
+						                    - ((double) maxSize.longer
+								                        /maxSize.shorter));
+				               System.out.println("AspectTolerance");
+				               System.out.println(d);
+				               return d < ASPECT_TOLERANCE;})
+		               .findFirst()
+                       .orElse(new SmartSize(0,0))
+				       .size;
+		return size3;
+	}
+	
+	private void transform(int width, int height){
+		Matrix matrix = new Matrix();
+		int rotation = ((WindowManager) fullscreenActivity.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
+		RectF textureRectF = new RectF(0, 0, width, height);
+		RectF previewRectF = new RectF(0, 0, textureView.getHeight(), textureView.getWidth());
+		
+		float centerX = textureRectF.centerX();
+		float centerY = textureRectF.centerY();
+		
+		if(rotation == Surface.ROTATION_90
+		   || rotation == Surface.ROTATION_270){
+			previewRectF.offset(centerX - previewRectF.centerX(),
+			                    centerY - previewRectF.centerY());
+			matrix.setRectToRect(textureRectF, previewRectF, Matrix.ScaleToFit.FILL);
+			float scale = Math.max((float) width / textureView.getWidth(), (float) height / textureView.getHeight());
+			matrix.postScale(scale, scale, centerX, centerY);
+			matrix.postRotate(90 * (rotation - 2), centerX, centerY);
+		}
+		
+		textureView.setTransform(matrix);
 	}
 }
